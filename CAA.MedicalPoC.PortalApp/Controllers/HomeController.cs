65a73605a296bd1d1089f72635e77fc4c55bf038
odee30 <odee30@gmail.com>
2018-09-08 14:34:22 +0100

    initial

--- CAA.MedicalPoC.PortalApp/Controllers/HomeController.cs
+++ CAA.MedicalPoC.PortalApp/Controllers/HomeController.cs
@@ -1,14 +1,43 @@
-﻿using System;
+﻿using Microsoft.Owin.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
 namespace CAA.MedicalPoC.PortalApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
+        {
+            ViewBag.ClaimsIdentity = System.Threading.Thread.CurrentPrincipal.Identity;
+            var claimsIdentity = System.Threading.Thread.CurrentPrincipal.Identity as ClaimsIdentity;
+            ViewBag.DisplayName = claimsIdentity.Claims.First(c => c.Type == ClaimTypes.GivenName).Value;
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var owinContext = this.Request.GetOwinContext();
+                var authProperties = new AuthenticationProperties();
+                authProperties.RedirectUri = new Uri(this.HttpContext.Request.Url, new UrlHelper(this.ControllerContext.RequestContext).Action("PostLogOff")).AbsoluteUri;
+                owinContext.Authentication.SignOut(authProperties);
+                return View();
+            }
+            else
+            {
+                throw new InvalidOperationException("User is not authenticated");
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult PostLogOff()
         {
             return View();
         }
---
+++ CAA.MedicalPoC.PortalApp/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+
+[assembly: OwinStartup(typeof(CAA.MedicalPoC.PortalApp.Startup))]
+
+namespace CAA.MedicalPoC.PortalApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(
+            new CookieAuthenticationOptions
+            {
+                AuthenticationType = CookieAuthenticationDefaults.AuthenticationType
+            });
+            app.UseWsFederationAuthentication(
+            new WsFederationAuthenticationOptions
+            {
+                //MetadataAddress = "https://login.microsoftonline.com/c4edd5ba-10c3-4fe3-946a-7c9c446ab8c8/federationmetadata/2007-06/federationmetadata.xml",
+                MetadataAddress = "https://sts.caalab.co.uk/federationmetadata/2007-06/federationmetadata.xml",
+                Wtrealm = "http://localhost:51571/",
+            });
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
